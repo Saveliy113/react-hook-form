223ad94b7825fd84ed8cd9f762621306767555cc
Saveliy113 <sava2698@gmail.com>
2023-05-11 21:45:14 +0600

    Router connected

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
+//React
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {
+  BrowserRouter,
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom';
+
+//Components
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Step1 from './components/Step1';
+import Step2 from './components/Step2';
+import Step3 from './components/Step3';
 
+//CSS
+import './index.css';
+
+const router = createBrowserRouter([
+  { path: '/', element: <Step1 /> },
+  { path: '/step2', element: <Step2 /> },
+  { path: '/step3', element: <Step3 /> },
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<RouterProvider router={router} />);
